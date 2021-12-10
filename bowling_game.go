c13f58f7f8ca9aac0512b82741f6790a6a657519
Lorenzo Benedetto <lorenzo.benedetto@prontopro.it>
2021-12-10 11:17:51 +0100

    Lore B

--- bowling_game.go
+++ bowling_game.go
@@ -9,22 +9,41 @@ type Game interface {
 
 type BowlingGame struct {
 	score int
-	currentFrame Frame
+	CurrentFrame Frame
 }
 
 type Frame struct {
-	firstS
+	AvailableRolls int
 }
 
 func (b *BowlingGame) Roll(pins int) error {
+	if b.CurrentFrame.AvailableRolls == 0 {
+		return fmt.Errorf("can't roll more times")
+	}
+
 	if pins > 10 {
 		return fmt.Errorf("more than 10 pins are not allowed")
 	}
 
+	b.CurrentFrame.AvailableRolls -= 1
+
+	if pins == 10 {
+		b.CurrentFrame.AvailableRolls = 0
+	}
+
 	b.score += pins
+
 	return nil
 }
 
 func (b *BowlingGame) Score() int {
 	return b.score
+}
+
+func NewBowlingGame() BowlingGame {
+	return BowlingGame{
+		CurrentFrame: Frame{
+			AvailableRolls: 2,
+		},
+	}
 }
--- bowling_game_test.go
+++ bowling_game_test.go
@@ -6,7 +6,7 @@ func TestBowlingGame(t *testing.T) {
 	t.Run(
 		"When we start a new game AND we roll once THEN the score is equal to the roll",
 		func(t *testing.T) {
-			bowlingGame := BowlingGame{}
+			bowlingGame := NewBowlingGame()
 
 			bowlingGame.Roll(1)
 
@@ -19,7 +19,7 @@ func TestBowlingGame(t *testing.T) {
 	t.Run(
 		"When we start a new game AND we roll twice THEN the score is equal to the sum of rolls",
 		func(t *testing.T) {
-			bowlingGame := BowlingGame{}
+			bowlingGame := NewBowlingGame()
 
 			bowlingGame.Roll(1)
 			bowlingGame.Roll(1)
@@ -33,7 +33,7 @@ func TestBowlingGame(t *testing.T) {
 	t.Run(
 		"When we roll AND we roll a number of pin over 10 THEN error",
 		func(t *testing.T) {
-			bowlingGame := BowlingGame{}
+			bowlingGame := NewBowlingGame()
 
 			if bowlingGame.Roll(11) == nil {
 				t.Fail()
@@ -44,7 +44,7 @@ func TestBowlingGame(t *testing.T) {
 	t.Run(
 		"When we roll a strike (10 pins) THEN we can not roll again in the same frame",
 		func(t *testing.T) {
-			bowlingGame := BowlingGame{}
+			bowlingGame := NewBowlingGame()
 
 			bowlingGame.Roll(10)
 
