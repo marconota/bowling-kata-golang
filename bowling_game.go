20750b63058e1408384fbf9e1776fb0ca2d34fc9
lorenzoranucci <lorenzofranco.ranucci@gmail.com>
2021-12-10 11:02:21 +0100

    Proto

--- bowling_game.go
+++ bowling_game.go
@@ -3,23 +3,28 @@ package main
 import "fmt"
 
 type Game interface {
-    Roll(int) error
-    Score() int
+	Roll(int) error
+	Score() int
 }
 
 type BowlingGame struct {
-    score int
+	score int
+	currentFrame Frame
+}
+
+type Frame struct {
+	firstS
 }
 
 func (b *BowlingGame) Roll(pins int) error {
-    if pins > 10 {
-        return fmt.Errorf("more than 10 pins are not allowed")
-    }
+	if pins > 10 {
+		return fmt.Errorf("more than 10 pins are not allowed")
+	}
 
-    b.score += pins
-    return nil
+	b.score += pins
+	return nil
 }
 
 func (b *BowlingGame) Score() int {
-    return b.score
+	return b.score
 }
--- bowling_game_test.go
+++ bowling_game_test.go
@@ -3,40 +3,55 @@ package main
 import "testing"
 
 func TestBowlingGame(t *testing.T) {
-	t.Run("When we start a new game AND we roll once THEN the score is equal to the roll", func(t *testing.T) {
-		bowlingGame := BowlingGame{}
+	t.Run(
+		"When we start a new game AND we roll once THEN the score is equal to the roll",
+		func(t *testing.T) {
+			bowlingGame := BowlingGame{}
 
-		bowlingGame.Roll(1)
+			bowlingGame.Roll(1)
 
-		if bowlingGame.Score() != 1 {
-			t.Fail()
-		}
-    })
+			if bowlingGame.Score() != 1 {
+				t.Fail()
+			}
+		},
+	)
 
-	t.Run("When we start a new game AND we roll twice THEN the score is equal to the sum of rolls", func(t *testing.T) {
-		bowlingGame := BowlingGame{}
+	t.Run(
+		"When we start a new game AND we roll twice THEN the score is equal to the sum of rolls",
+		func(t *testing.T) {
+			bowlingGame := BowlingGame{}
 
-		bowlingGame.Roll(1)
-		bowlingGame.Roll(1)
+			bowlingGame.Roll(1)
+			bowlingGame.Roll(1)
 
-		if bowlingGame.Score() != 2 {
-			t.Fail()
-		}
-	})
+			if bowlingGame.Score() != 2 {
+				t.Fail()
+			}
+		},
+	)
 
-	t.Run("When we roll AND we roll a number of pin over 10 THEN error", func(t *testing.T) {
-		bowlingGame := BowlingGame{}
+	t.Run(
+		"When we roll AND we roll a number of pin over 10 THEN error",
+		func(t *testing.T) {
+			bowlingGame := BowlingGame{}
 
-		if bowlingGame.Roll(11) == nil {
-			t.Fail()
-		}
-	})
+			if bowlingGame.Roll(11) == nil {
+				t.Fail()
+			}
+		},
+	)
 
-	t.Run("When we roll a strike (10 pins) THEN we can not roll again in the same frame", func(t *testing.T) {
-		bowlingGame := BowlingGame{}
+	t.Run(
+		"When we roll a strike (10 pins) THEN we can not roll again in the same frame",
+		func(t *testing.T) {
+			bowlingGame := BowlingGame{}
 
-		bowlingGame.Roll(10)
+			bowlingGame.Roll(10)
 
-	})
-
+			err := bowlingGame.Roll(1)
+			if err == nil {
+				t.Fail()
+			}
+		},
+	)
 }
